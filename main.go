d3408a5f5d0719d2b0e6fa6edc24ed9247ddf2c8
Matthew Newman <theMatthewNewman@gmail.com>
2024-03-18 12:25:11 -0400

    added method handling

--- main.go
+++ main.go
@@ -18,9 +18,21 @@ func getHello(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello, HTTP!\n")
 }
 
+func methodHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		fmt.Fprintf(w, "GET request received \n")
+	case "POST":
+		fmt.Fprintf(w, "POST request received \n")
+	default:
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
+}
+
 func main() {
 	http.HandleFunc("/", getRoot)
 	http.HandleFunc("/hello", getHello)
+	http.HandleFunc("/method", methodHandler)
 	err := http.ListenAndServe(":3333", nil)
 	if errors.Is(err, http.ErrServerClosed) {
 		fmt.Printf("Server Closed \n")
