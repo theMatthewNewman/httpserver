fb2f70b5414562ca9835bf7b42e6f7b06c71f709
Matthew Newman <theMatthewNewman@gmail.com>
2024-03-19 09:53:43 -0400

    added albums

--- main.go
+++ main.go
@@ -1,12 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
 )
+
+// represents data about album.
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+// albums slice to seed record album data
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func getAlbums(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Returning all albums")
+	json.NewEncoder(w).Encode(albums)
+}
 
 func getRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Printf("Got / request \n")
@@ -19,6 +40,7 @@ func getHello(w http.ResponseWriter, r *http.Request) {
 }
 
 func methodHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("Got /method request \n")
 	switch r.Method {
 	case "GET":
 		fmt.Fprintf(w, "GET request received \n")
@@ -33,6 +55,7 @@ func main() {
 	http.HandleFunc("/", getRoot)
 	http.HandleFunc("/hello", getHello)
 	http.HandleFunc("/method", methodHandler)
+	http.HandleFunc("/albums", getAlbums)
 	err := http.ListenAndServe(":3333", nil)
 	if errors.Is(err, http.ErrServerClosed) {
 		fmt.Printf("Server Closed \n")
